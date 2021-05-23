aa9b1c282a1e3198d5a5cb2af2e74b643b7b77e1
Marius Orcsik <marius@habarnam.ro>
2021-05-23 16:03:29 +0200

    Moved functionality outside of the cmd main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,54 +1,21 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"net/http"
-
 	ap "github.com/go-ap/activitypub"
 	"github.com/go-ap/client"
-	h "github.com/go-ap/handlers"
 	"github.com/mariusor/spammy"
 	"github.com/sirupsen/logrus"
-	"golang.org/x/oauth2"
 )
 
 
 var (
 	logger = logrus.New()
 	fedbox client.ActivityPub = nil
-	ServiceAPI  = ap.IRI("https://fedbox.local")
-
-	OAuthKey    = "aa52ae57-6ec6-4ddd-afcc-1fcbea6a29c0"
-	OAuthSecret = "asd"
 )
 
-func config() oauth2.Config {
-	return oauth2.Config{
-		ClientID:     OAuthKey,
-		ClientSecret: OAuthSecret,
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  fmt.Sprintf("%s/oauth/authorize", ServiceAPI),
-			TokenURL: fmt.Sprintf("%s/oauth/token", ServiceAPI),
-		},
-		RedirectURL: fmt.Sprintf("https://brutalinks.local/auth/fedbox/callback"),
-	}
-}
-func SelfIRI() ap.IRI {
-	return spammy.Actors.IRI(ServiceAPI).AddPath(OAuthKey)
-}
-
-func self() ap.Actor {
-	self := SelfIRI()
-	return ap.Application{
-		ID:     self,
-		Type:   ap.ApplicationType,
-		Outbox: h.Outbox.IRI(self),
-		Inbox:  h.Inbox.IRI(self),
-	}
-}
 func fields(c ...client.Ctx) logrus.Fields {
 	cc := make(logrus.Fields)
 	for _, ctx := range c {
@@ -71,135 +38,29 @@ func errf(c ...client.Ctx) client.LogFn {
 	return logFn(c...).Errorf
 }
 
-func C2SSign() client.RequestSignFn {
-	var tok *oauth2.Token
-	config := config()
-	return func(req *http.Request) error {
-		if tok == nil {
-			var err error
-			tok, err = config.PasswordCredentialsToken(context.Background(), fmt.Sprintf("oauth-client-app-%s", OAuthKey), config.ClientSecret)
-			if err != nil {
-				return err
-			}
-		}
-		tok.SetAuthHeader(req)
-		return nil
-	}
-}
-
-func ExecActivity(act ap.Item, parent ap.Item) (ap.Item, error) {
-	iri, ff, err := fedbox.ToCollection(h.Outbox.IRI(parent), act)
-	if err != nil {
-		return nil, err
-	}
-	if len(iri) > 0 {
-		return fedbox.LoadIRI(iri)
-	}
-	fmt.Printf("%v", ff)
-	return nil, nil
-}
-
-func CreateActivity(ob ap.Item, parent ap.Item) (ap.Item, error) {
-	create := ap.Create{
-		Type:   ap.CreateType,
-		Object: ob,
-		To:     ap.ItemCollection{ServiceAPI, ap.PublicNS},
-		Actor: parent,
-	}
-	iri, final, err := fedbox.ToCollection(h.Outbox.IRI(parent), create)
-	if err != nil {
-		return final, err
-	}
-	it, err := fedbox.LoadIRI(iri)
-	if err != nil {
-		return nil, err
-	}
-	if j, err := json.Marshal(it); err == nil {
-		fmt.Printf("Activity: %s\n", j)
-	}
-	return final, nil
-}
-
-func exec(cnt int, actFn func(ap.Item, ap.Item) (ap.Item, error), itFn func() ap.Item) (map[ap.IRI]ap.Item, error) {
-	result := make(map[ap.IRI]ap.Item)
-	for i := 0; i < cnt; i++ {
-		it := itFn()
-		var parent ap.Item
-		ap.OnObject(it, func(o *ap.Object) error {
-			parent = o.AttributedTo
-			return nil
-		})
-		ob, err := actFn(it, parent)
-		if err != nil {
-			errf()("Error: %s", err)
-			break
-		}
-		result[ob.GetLink()] = ob
-	}
-	return result, nil
-}
-
-func CreateRandomActors(cnt int) (map[ap.IRI]ap.Item, error) {
-	return exec(cnt, CreateActivity, func() ap.Item {
-		return  spammy.RandomActor(self())
-	})
-}
-
-func CreateRandomObjects(cnt int, actors map[ap.IRI]ap.Item) (map[ap.IRI]ap.Item, error) {
-	return exec(cnt, CreateActivity, func() ap.Item {
-		return spammy.RandomObject(self())
-	})
-}
-
-func CreateRandomActivities(cnt int, objects map[ap.IRI]ap.Item, actors map[ap.IRI]ap.Item) (map[ap.IRI]ap.Item, error) {
-	iris := make([]ap.IRI, len(objects))
-	i := 0
-	for iri, it := range objects {
-		if it.GetType() == ap.TombstoneType {
-			continue
-		}
-		iris[i] = iri
-		i++
-	}
-	result := make(map[ap.IRI]ap.Item)
-	for _, iri := range iris {
-		parent := spammy.GetRandomItemFromMap(actors)
-		actRes, err := exec(cnt, ExecActivity, func() ap.Item {
-			act := spammy.RandomActivity(iri, parent)
-			act.CC = append(act.CC, self())
-			return act
-		})
-		if err != nil {
-			errf()("Error: %s", err)
-			continue
-		}
-		for k, v := range actRes {
-			result[k] = v
-		}
-	}
-	return result, nil
-}
-
 func main() {
-	serv := flag.String("url", ServiceAPI.String(), "The FedBOX url to connect to")
-	key := flag.String("client", OAuthKey, "The FedBOX application uuid")
-	secret := flag.String("secret", OAuthSecret, "The FedBOX application secret")
+	serv := flag.String("url", spammy.ServiceAPI.String(), "The FedBOX url to connect to")
+	key := flag.String("client", spammy.OAuthKey, "The FedBOX application uuid")
+	secret := flag.String("secret", spammy.OAuthSecret, "The FedBOX application secret")
 	flag.Parse()
 	if serv != nil {
-		ServiceAPI = ap.IRI(*serv)
+		spammy.ServiceAPI = ap.IRI(*serv)
 	}
 	if key != nil {
-		OAuthKey = *key
+		spammy.OAuthKey = *key
 	}
 	if secret != nil {
-		OAuthSecret = *secret
+		spammy.OAuthSecret = *secret
 	}
-	fedbox = client.New(
+
+	spammy.FedBOX = client.New(
 		client.TLSConfigSkipVerify(),
-		client.SignFn(C2SSign()),
+		client.SignFn(spammy.C2SSign()),
 		client.SetErrorLogger(errf),
 		client.SetInfoLogger(infof),
 	)
+	spammy.ErrFn = errf
+
 	printItems := func(items map[ap.IRI]ap.Item) {
 		for _, it := range items {
 			if j, err := json.Marshal(it); err == nil {
@@ -207,10 +68,10 @@ func main() {
 			}
 		}
 	}
-	actors, _ := CreateRandomActors(20)
+	actors, _ := spammy.CreateRandomActors(20)
 	printItems(actors)
-	objects, _ := CreateRandomObjects(100, actors)
+	objects, _ := spammy.CreateRandomObjects(100, actors)
 	printItems(objects)
-	activities, _ := CreateRandomActivities(50, objects, actors)
+	activities, _ := spammy.CreateRandomActivities(50, objects, actors)
 	printItems(activities)
 }
--- spammer.go
+++ spammer.go
@@ -2,7 +2,10 @@ package spammy
 
 import (
 	"bytes"
+	"context"
 	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"mime"
@@ -12,29 +15,39 @@ import (
 
 	"github.com/docker/docker/pkg/namesgenerator"
 	ap "github.com/go-ap/activitypub"
+	"github.com/go-ap/client"
 	h "github.com/go-ap/handlers"
+	"golang.org/x/oauth2"
 )
 
 const (
 	Actors h.CollectionType = "actors"
-	ServiceAPI ap.IRI = ""
 )
 
-var availableExtensions = [...]string{
-	// text
-	"html",
-	"txt",
-	"md",
-	// document?
-	"svg",
-	// image
-	"jpg",
-	"png",
-	// audio
-	"mp3",
-	// video
-	"webm",
-}
+var (
+	ServiceAPI  = ap.IRI("https://FedBOX.local")
+
+	OAuthKey    = "aa52ae57-6ec6-4ddd-afcc-1fcbea6a29c0"
+	OAuthSecret = "asd"
+	FedBOX      client.ActivityPub = nil
+	ErrFn        func(c ...client.Ctx) client.LogFn
+
+	availableExtensions = [...]string{
+		// text
+		"html",
+		"txt",
+		"md",
+		// document?
+		"svg",
+		// image
+		"jpg",
+		"png",
+		// audio
+		"mp3",
+		// video
+		"webm",
+	}
+)
 
 func init() {
 	data.Walk("data/", func(path string, info os.FileInfo, err error) error {
@@ -215,4 +228,139 @@ func RandomActivity(ob ap.Item, parent ap.Item) *ap.Activity {
 	act.To = ap.ItemCollection{parent.GetLink(), ap.PublicNS}
 
 	return act
+}
+
+func self() ap.Actor {
+	self := SelfIRI()
+	return ap.Application{
+		ID:     self,
+		Type:   ap.ApplicationType,
+		Outbox: h.Outbox.IRI(self),
+		Inbox:  h.Inbox.IRI(self),
+	}
+}
+
+func SelfIRI() ap.IRI {
+	return Actors.IRI(ServiceAPI).AddPath(OAuthKey)
+}
+
+func config() oauth2.Config {
+	return oauth2.Config{
+		ClientID:     OAuthKey,
+		ClientSecret: OAuthSecret,
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  fmt.Sprintf("%s/oauth/authorize", ServiceAPI),
+			TokenURL: fmt.Sprintf("%s/oauth/token", ServiceAPI),
+		},
+		RedirectURL: fmt.Sprintf("https://brutalinks.local/auth/FedBOX/callback"),
+	}
+}
+
+func C2SSign() client.RequestSignFn {
+	var tok *oauth2.Token
+	config := config()
+	return func(req *http.Request) error {
+		if tok == nil {
+			var err error
+			tok, err = config.PasswordCredentialsToken(context.Background(), fmt.Sprintf("oauth-client-app-%s", OAuthKey), config.ClientSecret)
+			if err != nil {
+				return err
+			}
+		}
+		tok.SetAuthHeader(req)
+		return nil
+	}
+}
+
+func ExecActivity(act ap.Item, parent ap.Item) (ap.Item, error) {
+	iri, ff, err := FedBOX.ToCollection(h.Outbox.IRI(parent), act)
+	if err != nil {
+		return nil, err
+	}
+	if len(iri) > 0 {
+		return FedBOX.LoadIRI(iri)
+	}
+	fmt.Printf("%v", ff)
+	return nil, nil
+}
+
+func CreateActivity(ob ap.Item, parent ap.Item) (ap.Item, error) {
+	create := ap.Create{
+		Type:   ap.CreateType,
+		Object: ob,
+		To:     ap.ItemCollection{ServiceAPI, ap.PublicNS},
+		Actor: parent,
+	}
+	iri, final, err := FedBOX.ToCollection(h.Outbox.IRI(parent), create)
+	if err != nil {
+		return final, err
+	}
+	it, err := FedBOX.LoadIRI(iri)
+	if err != nil {
+		return nil, err
+	}
+	if j, err := json.Marshal(it); err == nil {
+		fmt.Printf("Activity: %s\n", j)
+	}
+	return final, nil
+}
+
+func exec(cnt int, actFn func(ap.Item, ap.Item) (ap.Item, error), itFn func() ap.Item) (map[ap.IRI]ap.Item, error) {
+	result := make(map[ap.IRI]ap.Item)
+	for i := 0; i < cnt; i++ {
+		it := itFn()
+		var parent ap.Item
+		ap.OnObject(it, func(o *ap.Object) error {
+			parent = o.AttributedTo
+			return nil
+		})
+		ob, err := actFn(it, parent)
+		if err != nil {
+			ErrFn()("Error: %s", err)
+			break
+		}
+		result[ob.GetLink()] = ob
+	}
+	return result, nil
+}
+
+func CreateRandomActors(cnt int) (map[ap.IRI]ap.Item, error) {
+	return exec(cnt, CreateActivity, func() ap.Item {
+		return  RandomActor(self())
+	})
+}
+
+func CreateRandomObjects(cnt int, actors map[ap.IRI]ap.Item) (map[ap.IRI]ap.Item, error) {
+	return exec(cnt, CreateActivity, func() ap.Item {
+		return RandomObject(self())
+	})
+}
+
+func CreateRandomActivities(cnt int, objects map[ap.IRI]ap.Item, actors map[ap.IRI]ap.Item) (map[ap.IRI]ap.Item, error) {
+	iris := make([]ap.IRI, len(objects))
+	i := 0
+	for iri, it := range objects {
+		if it.GetType() == ap.TombstoneType {
+			continue
+		}
+		iris[i] = iri
+		i++
+	}
+	result := make(map[ap.IRI]ap.Item)
+	for _, iri := range iris {
+		parent := GetRandomItemFromMap(actors)
+		actRes, err := exec(cnt, ExecActivity, func() ap.Item {
+			act := RandomActivity(iri, parent)
+			act.CC = append(act.CC, self())
+			return act
+		})
+		if err != nil {
+			ErrFn()("Error: %s", err)
+			continue
+		}
+		for k, v := range actRes {
+			result[k] = v
+		}
+	}
+	return result, nil
 }
